d62c1cde5eb9111f94cca102fb2ad6d9df8fd3d6
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-22 14:12:02 +0200

    temp fixe

--- client.py
+++ client.py
@@ -27,8 +27,13 @@ def receive():
 
 def write():
     while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode("ascii"))
+        message = input("")
+        if message.lower() == "quit":
+            client.send("QUIT".encode("ascii"))
+            client.close()
+            break
+        else:
+            client.send(f"MSG:{message}".encode("ascii"))
 
 
 recive_thread = threading.Thread(target=receive)
@@ -36,3 +41,42 @@ recive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
+# import socket
+# import threading
+
+# nickname = input("Choose your nickname: ")
+
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(("127.0.0.1", 55555))
+
+
+# def receive():
+#     while True:
+#         try:
+#             message = client.recv(1024).decode("ascii")
+#             if message == "NICK":
+#                 client.send(nickname.encode("ascii"))
+#             else:
+#                 print(message)
+#                 if message.lower() == "quit":
+#                     client.close()
+#                     break
+
+#         except:
+#             print("ERROR!")
+#             client.close()
+#             break
+
+
+# def write():
+#     while True:
+#         message = f'{nickname}: {input("")}'
+#         client.send(message.encode("ascii"))
+
+
+# recive_thread = threading.Thread(target=receive)
+# recive_thread.start()
+
+# write_thread = threading.Thread(target=write)
+# write_thread.start()
--- server.py
+++ server.py
@@ -12,44 +12,117 @@ clients = []
 nicknames = []
 
 
-def broadcast(message):
-	for client in clients:
-		client.send(message)
+def broadcast(message, sender=None):
+    for client in clients:
+        if client != sender:
+            client.send(message)
 
 
 def handle(client):
-	while True:
-		try:
-			message = client.recv(1024)
-			broadcast(message)
-		except:
-			index = clients.index(client)
-			clients.remove(client)
-			client.close()
-			nickname = nicknames[index]
-			broadcast(f"{nickname} left the chat!".encode("ascii"))
-			nicknames.remove(nickname)
-
-			break
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            if message.startswith("MSG:"):
+                broadcast(message[4:].encode("ascii"), client)
+            elif message.startswith("QUIT"):
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f"{nickname} left the chat!".encode("ascii"), None)
+                nicknames.remove(nickname)
+                break
+            elif message.startswith("JOIN:"):
+                nickname = message[5:]
+                nicknames.append(nickname)
+                clients.append(client)
+                print(f"User {nickname} joined the chat!")
+                broadcast(f"{nickname} joined the chat!".encode("ascii"), client)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f"{nickname} left the chat!".encode("ascii"), None)
+            nicknames.remove(nickname)
+            break
 
 
 def receive():
-	while True:
-		client, address = server.accept()
-		print(f"Connected with {str(address)}")
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
 
-		client.send("NICK".encode("ascii"))
-		nickname = client.recv(1024).decode("ascii")
-		nicknames.append(nickname)
-		clients.append(client)
+        client.send("NICK".encode("ascii"))
+        nickname = client.recv(1024).decode("ascii")
+        nicknames.append(nickname)
+        clients.append(client)
 
-		print(f"Nickname of the client is {nickname}!")
-		broadcast(f"{nickname} joined the chat!".encode("ascii"))
-		client.send("Connected to the server! (type 'quit' to exit from chat)".encode("ascii"))
+        print(f"User {nickname} joined the chat!")
+        broadcast(f"{nickname} joined the chat!".encode("ascii"), client)
+        client.send(
+            "Connected to the server! (type 'quit' to exit from chat)".encode("ascii")
+        )
 
-		thread = threading.Thread(target=handle, args=(client,))
-		thread.start()
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
 
 print("Server is listening...")
 receive()
+
+# import socket
+# import threading
+
+# host = "127.0.0.1"  # localhost
+# port = 55555
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((host, port))
+# server.listen()
+
+# clients = []
+# nicknames = []
+
+
+# def broadcast(message):
+# 	for client in clients:
+# 		client.send(message)
+
+
+# def handle(client):
+# 	while True:
+# 		try:
+# 			message = client.recv(1024)
+# 			broadcast(message)
+# 		except:
+# 			index = clients.index(client)
+# 			clients.remove(client)
+# 			client.close()
+# 			nickname = nicknames[index]
+# 			broadcast(f"{nickname} left the chat!".encode("ascii"))
+# 			nicknames.remove(nickname)
+
+# 			break
+
+
+# def receive():
+# 	while True:
+# 		client, address = server.accept()
+# 		print(f"Connected with {str(address)}")
+
+# 		client.send("NICK".encode("ascii"))
+# 		nickname = client.recv(1024).decode("ascii")
+# 		nicknames.append(nickname)
+# 		clients.append(client)
+
+# 		print(f"User {nickname} joined the chat!")
+# 		broadcast(f"{nickname} joined the chat!".encode("ascii"))
+# 		client.send("Connected to the server! (type 'quit' to exit from chat)".encode("ascii"))
+
+# 		thread = threading.Thread(target=handle, args=(client,))
+# 		thread.start()
+
+
+# print("Server is listening...")
+# receive()
